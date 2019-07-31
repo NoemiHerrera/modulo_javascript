a0d093f4ddaa6c9266fcac4566f5791693230f64
Noemi <mimihcortes@hotmail.com>
2019-07-31 18:00:19 -0500

    first classof  js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+console.log ('Hola mundo from JS')
+//desde tu edad cuantos días has vivido
+
+ 
+console.log('Mi edad en días es: 50 * 365')
+var age = 22
+
+365 * 22
+var livingDays = 365 * 22
+
+//Tenemos una memoria usb de 4 gb, cuantas fotos de 3mb le caben
+
+var gb =  1024
+var gb 1024 * 3
+
+//Tenemos  22 cubiculos, en cada cubiculos podemos meter 3 carros y 2 camionetas.
+//cuantos cubiculos llenamos completos
+//cuantas camionets sobran
+//cuantos carros sobran
+
+//57 camionetas
+//130 carros
+
+var totalParkingCars = parkingSpace * 3
+var totalParkingSuvs = totalParkingSuvs * 2
+
+console.log(
+    'Full house ?',
+)
+

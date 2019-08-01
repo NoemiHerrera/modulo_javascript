201b3967457fbd1d6ef8e549189ff37147ce1ba5
Noemi <mimihcortes@hotmail.com>
2019-08-01 16:42:45 -0500

    second-class

---
+++ js/main.js
@@ -0,0 +1,97 @@
+console.log ('Hola mundo from JS')
+//desde tu edad cuantos días has vivido
+
+ 
+console.log('Mi edad en días es: 50 * 365')
+var age = 22
+
+365 * 22
+var livingDays = 365 * 22
+
+//Tenemos una memoria usb de 4 gb, cuantas fotos de 3mb le caben
+
+var gb =  1024 * 4
+var fotos= gb/3
+
+//Tenemos  22 cubiculos, en cada cubiculos podemos meter 3 carros y 2 camionetas.
+//cuantos cubiculos llenamos completos
+//cuantas camionetas sobran
+//cuantos carros sobran
+
+//57 camionetas
+//130 carros
+var parkingSpace = 22
+var cars = 130
+var suvs = 57
+​
+var totalParkingCars = parkingSpace * 3
+var totalParkingSuvs = parkingSpace * 2
+​
+console.log(
+  'Full house ?', (
+    (totalParkingCars < cars) && (totalParkingSuvs < suvs)
+  )
+)
+​
+console.log(totalParkingCars - cars)
+console.log(totalParkingSuvs - suvs)
+Contraer
+
+
+var myPet = 'Sr. Peabody'
+let myPet = 'Misifus'
+
+const NAME = 'David'
+
+//Cual es el área de un circulo , cuyo diametro es 10cm
+
+const pi= 3.1416 
+const DIAMETER = 10
+const RADIUS = DIAMETER/2
+
+const AREA = (PI)* (RADIUS**2)
+
+/*var number = 1
+if (number === '1') {
+
+} else if () {
+    console.log ('El 1 no es numerico o es invalido')
+}*/
+
+if (number === '1') {
+    console.log('si es 1')
+} else if (typeof number != 'number') {
+    console.log('su valor no es numerico')
+}else {
+
+}
+
+//Dado un número {number}, verificar si es par
+
+    var num = 10
+    if (num % 2 ==0) {
+    console.log ('es un número par')
+    } else if (num % 2 == 0){
+        console.log('no es un numero par')
+    } else if ('imprime ')
+
+
+//saber si el producto de dos números es primo y mayor a 40
+//validar que los numeros sean digitos
+//{number one} {number two}
+
+//se necesita realizarlo con ciclos
+
+var firstNumber = 
+
+
+
+
+
+
+
+
+
+
+
+
